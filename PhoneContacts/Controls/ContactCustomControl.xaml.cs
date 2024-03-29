3113d2389b73cf59ce875a5d2e1936e2f0715ee0
Алексей <com.twin@yandex.ru>
2024-03-29 16:29:50 +0700

    feature(View): add custom control for contact info

---
+++ PhoneContacts/Controls/ContactCustomControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PhoneContacts.Model.Services;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PhoneContacts.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ContactCustomControl.xaml
+    /// </summary>
+    public partial class ContactCustomControl : UserControl
+    {
+        public ContactCustomControl()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PhoneTextBox.CaretIndex = PhoneTextBox.Text.Length;
+        }
+
+        private void PhoneTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            int value;
+            if (!Int32.TryParse(e.Text, out value))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void PhoneTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                string pastedText = (string)e.DataObject.GetData(typeof(string));
+                if (!ValueValidator.ValidateNumber(pastedText))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
--- PhoneContacts/MainWindow.xaml.cs
+++ PhoneContacts/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using PhoneContacts.Model.Services;
-using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
+﻿using System.Windows;
 
 namespace PhoneContacts
 {
@@ -14,36 +10,6 @@ namespace PhoneContacts
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void PhoneTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            int value;
-            if (!Int32.TryParse(e.Text, out value))
-            {
-                e.Handled = true;
-            }
-        }
-
-        private void PhoneTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
-            {
-                string pastedText = (string)e.DataObject.GetData(typeof(string));
-                if (!ValueValidator.ValidateNumber(pastedText))
-                {
-                    e.CancelCommand();
-                }
-            }
-            else
-            {
-                e.CancelCommand();
-            }
-        }
-
-        private void PhoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            PhoneTextBox.CaretIndex = PhoneTextBox.Text.Length;
         }
     }
 }
